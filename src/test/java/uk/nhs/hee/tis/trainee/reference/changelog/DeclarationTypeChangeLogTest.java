6273bdc34e6128245f9ea9d7df9c94af78dfe09f
Andy Dingley <Judge40@users.noreply.github.com>
2021-04-19 16:55:24 +0100

    fix: Add DeclarationType values automatically
    The DeclarationType reference table is not populated automatically and
    requires manual intervention. Use Mongock to insert the missing data
    automatically.
    
    TIS21-1456

---
+++ src/test/java/uk/nhs/hee/tis/trainee/reference/changelog/DeclarationTypeChangeLogTest.java
@@ -0,0 +1,73 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright 2021 Crown Copyright (Health Education England)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package uk.nhs.hee.tis.trainee.reference.changelog;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.verify;
+
+import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.impl.MongockTemplate;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.nhs.hee.tis.trainee.reference.model.DeclarationType;
+
+@ExtendWith(MockitoExtension.class)
+class DeclarationTypeChangeLogTest {
+
+  private DeclarationTypeChangeLog changeLog;
+
+  @Mock
+  private MongockTemplate template;
+
+  @BeforeEach
+  void setUp() {
+    changeLog = new DeclarationTypeChangeLog();
+  }
+
+  @Test
+  void shouldAddInitialDeclarationTypes() {
+    changeLog.insertInitialDeclarationTypes(template);
+
+    ArgumentCaptor<Collection> collectionCaptor = ArgumentCaptor.forClass(Collection.class);
+    verify(template).insertAll(collectionCaptor.capture());
+
+    Collection<?> declarationTypes = collectionCaptor.getValue();
+    assertThat("Unexpected collection size.", declarationTypes.size(), is(3));
+    Set<String> declarationTypeValues = declarationTypes.stream()
+        .filter(dt -> dt instanceof DeclarationType)
+        .map(dt -> ((DeclarationType) dt).getLabel())
+        .collect(Collectors.toSet());
+
+    assertThat("Unexpected number of declaration type values.", declarationTypeValues.size(),
+        is(3));
+    assertThat("Unexpected declaration type values.", declarationTypeValues,
+        hasItems("Significant event", "Complaint", "Other investigation"));
+  }
+}
